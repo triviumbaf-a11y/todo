485d183d1d2acb65acb5f9fad0f8162239961a50
Andrey <triviumbaf@gmail.com>
2026-03-19 12:59:12 +0300

    Add simple connection

---
+++ postgres/connection/connection.go
@@ -0,0 +1,30 @@
+package connection
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx"
+	"log"
+)
+
+func CheckConnection() {
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(pgx.ConnConfig{
+		Host:     "localhost",
+		Port:     5432,
+		Database: "postgres",
+		User:     "postgres",
+		Password: "admin",
+	})
+
+	if err != nil {
+		log.Fatal("Error DB connection: ", error.Error(err))
+	}
+
+	if err = conn.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("DB connection successes")
+}
